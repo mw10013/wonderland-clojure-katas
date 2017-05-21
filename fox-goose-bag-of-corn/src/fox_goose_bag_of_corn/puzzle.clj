f2d9570170ace5a33776327c359e8ae95c890c10
Michael Wu <mw10013@gmail.com>
2017-05-20 22:00:19 -0400

    fox, goose, bag of corn

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,20 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[[:fox :goose :corn :you] [:boat] []]
+                [[:fox :corn] [:you :boat :goose] []]
+                [[:fox :corn] [:boat] [:you :goose]]
+                [[:fox :corn] [:you :boat] [:goose]]
+                [[:you :fox :corn] [:boat] [:goose]]
+                [[:fox] [:you :corn :boat] [:goose]]
+                [[:fox] [:boat] [:you :goose :corn]]
+                [[:fox] [:you :goose :boat] [:corn]]
+                [[:you :goose :fox] [:boat] [:corn]]
+                [[:goose] [:you :fox :boat] [:corn]]
+                [[:goose] [:boat] [:corn :you :fox]]
+                [[:goose] [:you :boat] [:corn :fox]]
+                [[:goose :you] [:boat] [:corn :fox]]
+                [[] [:goose :you :boat] [:corn :fox]]
+                [[] [:boat][:fox :goose :corn :you]]])
 
 (defn river-crossing-plan []
   start-pos)
