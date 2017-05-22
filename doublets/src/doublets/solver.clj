8f99cc40249b542cf9de810b4ac59a1326bd4668
Michael Wu <mw10013@gmail.com>
2017-05-22 08:33:55 -0400

    doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,36 @@
                (slurp)
                (read-string)))
 
+(defn neighbors [w]
+  (filter
+    #(and
+       (= (count %) (count w))
+       (= (count (remove true? (map = % w))) 1))
+    words))
+
+; (neighbors "bank")
+; (neighbors "bonk")
+; (neighbors "book")
+; (neighbors "door")
+; (neighbors "look")        
+; (neighbors "wheat")
+
+(defn walk [paths dest]
+  (if-let [p (first paths)]
+    (if (= (last p) dest)
+      p
+      (when-let [ns (neighbors (last p))]
+        (recur (concat
+                 (map (partial conj p) (remove (set p) ns))
+                 (rest paths))
+          dest)))
+    []))
+
+
 (defn doublets [word1 word2]
-  "make me work")
+  (walk [[word1]] word2))
+
+; (doublets "door" "lock")
+; (doublets "bank" "loan")
+; (doublets "wheat" "bread")
+; (doublets "ye" "freezer")
